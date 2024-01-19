7ef6ca480996fe9ed1f039aac352d03bfce5739a
Kbisa2039@gmail.com <118041554+udin12Q@users.noreply.github.com>
2024-01-19 15:58:06 +0800

    updated

--- lib/akun/setting.dart
+++ lib/akun/setting.dart
@@ -1,4 +1,5 @@
 import 'package:aaa/Home/HomePage.dart';
+import 'package:aaa/akun/tambah_akun.dart';
 import 'package:flutter/material.dart';
 
 class setting extends StatelessWidget {
@@ -57,6 +58,17 @@ class _WalletScreenState extends State<WalletScreen> {
             ListTile(
               title: Text('Tentang Kami'),
               onTap: () {},
+            ),
+            ListTile(
+              title: Text('Kebijakan Privasi'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: Text('Tambah akun'),
+              onTap: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => add()));
+              },
             ),
             Divider(),
             ElevatedButton(
---
+++ lib/akun/tambah_akun.dart
@@ -0,0 +1,71 @@
+import 'package:aaa/Home/HomePage.dart';
+import 'package:flutter/material.dart';
+
+class add extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void _loginPressed(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => HomeScreen()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                labelText: 'Email',
+              ),
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () => _loginPressed(context),
+              child: Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Halaman Utama'),
+      ),
+      body: Center(
+        child: Text('Selamat datang di Dompet Digital!'),
+      ),
+    );
+  }
+}
